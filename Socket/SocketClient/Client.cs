ced921c046b029af8cd38cd10455c531eeff030b
zhenglong2015 <853222835@qq.com>
2016-03-18 11:45:53 +0800

    修改客户端震动，增加捕获

--- Socket/SocketClient/Client.cs
+++ Socket/SocketClient/Client.cs
@@ -29,61 +29,74 @@ namespace SocketClient
         Socket socketSend;
         private void btnContent_Click(object sender, EventArgs e)
         {
-            //创建负责通信的socket
-            socketSend = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
-            IPAddress ip = IPAddress.Parse(txtIp.Text);
-            //获取要链接的服务器的Ip和端口号 
-            IPEndPoint point = new IPEndPoint(ip, Convert.ToInt32(txtPonit.Text));
-            socketSend.Connect(point);
-            ShowMsg(socketSend.RemoteEndPoint+":"+"连接成功！");
-            //开启一个线程不停的接收
+            try
+            {
+                //创建负责通信的socket
+                socketSend = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+                IPAddress ip = IPAddress.Parse(txtIp.Text);
+                //获取要链接的服务器的Ip和端口号 
+                IPEndPoint point = new IPEndPoint(ip, Convert.ToInt32(txtPonit.Text));
+                socketSend.Connect(point);
+                ShowMsg(socketSend.RemoteEndPoint + ":" + "连接成功！");
+                //开启一个线程不停的接收
 
-            Thread th = new Thread(Recviec);
-            th.IsBackground = true;
-            th.Start();
+                Thread th = new Thread(Recviec);
+                th.IsBackground = true;
+                th.Start();
+            }
+            catch (Exception)
+            {
+                
+            }
         }
-        
+
         /// <summary>
         /// 客户端接收服务器消息
         /// </summary>
         void Recviec()
         {
-            while (true)
-            {
-                byte[] buffer = new byte[1024 * 1024 * 3];
-                int r = socketSend.Receive(buffer);
-                if (r == 0) { break; }
-                if (buffer[0] == 0)//发送的文字消息
+            try{
+                while (true)
                 {
-                    string str = Encoding.UTF8.GetString(buffer, 1, r - 1);
-                    ShowMsg(socketSend.RemoteEndPoint + ":" + str);
-                }
-                else if (buffer[0] == 1)//发送的文件
-                {
-                    this.Invoke(new MethodInvoker(delegate ()
+                    byte[] buffer = new byte[1024 * 1024 * 3];
+                    int r = socketSend.Receive(buffer);
+                    if (r == 0) { break; }
+                    if (buffer[0] == 0)//发送的文字消息
                     {
-                        SaveFileDialog ssd = new SaveFileDialog();
-                        ssd.InitialDirectory = @"C:\Users\Administrator\Desktop";
-                        ssd.Title = "请选择要发送的文件";
-                        ssd.Filter = "所有文件|*.*";
-                        ssd.ShowDialog();
-                        string path = ssd.FileName;
-                        using (FileStream fs = new FileStream(path, FileMode.OpenOrCreate, FileAccess.Write))
+                        string str = Encoding.UTF8.GetString(buffer, 1, r - 1);
+                        ShowMsg(socketSend.RemoteEndPoint + ":" + str);
+                    }
+                    else if (buffer[0] == 1)//发送的文件
+                    {
+                        this.Invoke(new MethodInvoker(delegate ()
                         {
-                            fs.Write(buffer, 1, r - 1);
-                            MessageBox.Show("保存成功！");
-                        }
-                    }));
-                }
-                else if (buffer[0] == 2)//发送的震动
-                {
-                    for (int i = 0; i < 500; i++)
+                            SaveFileDialog ssd = new SaveFileDialog();
+                            ssd.InitialDirectory = @"C:\Users\Administrator\Desktop";
+                            ssd.Title = "请选择要发送的文件";
+                            ssd.Filter = "所有文件|*.*";
+                            ssd.ShowDialog();
+                            string path = ssd.FileName;
+                            using (FileStream fs = new FileStream(path, FileMode.OpenOrCreate, FileAccess.Write))
+                            {
+                                fs.Write(buffer, 1, r - 1);
+                                MessageBox.Show("保存成功！");
+                            }
+                        }));
+                    }
+                    else if (buffer[0] == 2)//发送的震动
                     {
-                        this.Location = new Point(200, 200);
-                        this.Location = new Point(230, 230);
+
+                        for (int i = 0; i < 3; i++)
+                        {
+                            Point p = this.FindForm().Location;
+                            this.FindForm().Location = new Point(p.X + 10, p.Y + 10);
+                            Thread.Sleep(30);
+                            this.FindForm().Location = p;
+                            Thread.Sleep(30);
+                        }
                     }
                 }
-            }
+            }catch { }
         }
 
 
--- Socket/SocketServer/Server.cs
+++ Socket/SocketServer/Server.cs
@@ -23,18 +23,25 @@ namespace SocketServer
 
         private void btnListen_Click(object sender, EventArgs e)
         {
-            //在服务端创建一个负责监听的Ip地址和端口的Socket
-            Socket socketWatch = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
-            IPAddress ip = IPAddress.Any;
-            //创建端口号对象
-            IPEndPoint point = new IPEndPoint(ip, Convert.ToInt32(txtPoint.Text));
-            socketWatch.Bind(point);
-            ShowMsg("监听成功！");
-            socketWatch.Listen(10);
+            try
+            {
+                //在服务端创建一个负责监听的Ip地址和端口的Socket
+                Socket socketWatch = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+                IPAddress ip = IPAddress.Any;
+                //创建端口号对象
+                IPEndPoint point = new IPEndPoint(ip, Convert.ToInt32(txtPoint.Text));
+                socketWatch.Bind(point);
+                ShowMsg("监听成功！");
+                socketWatch.Listen(10);
 
-            Thread th = new Thread(Listen);
-            th.IsBackground = true;
-            th.Start(socketWatch);
+                Thread th = new Thread(Listen);
+                th.IsBackground = true;
+                th.Start(socketWatch);
+            }
+            catch (Exception)
+            {
+
+            }
 
         }
         Dictionary<string, Socket> dic = new Dictionary<string, Socket>();
@@ -44,19 +51,25 @@ namespace SocketServer
         /// </summary>
         void Listen(object obj)
         {
-            Socket socketWatch = obj as Socket;
-            //等待客户端连接并且创建一个负责通信的socket
-            while (true)
+            try
             {
-                socketSend = socketWatch.Accept();
-                //将远程连接的Ip地址和连接存入集合中
-                dic.Add(socketSend.RemoteEndPoint.ToString(), socketSend);
-                cobIPList.Items.Add(socketSend.RemoteEndPoint.ToString());
-              //  ShowMsg(socketWatch.RemoteEndPoint.ToString() + ":" + "连接成功！");
+                Socket socketWatch = obj as Socket;
+                //等待客户端连接并且创建一个负责通信的socket
+                while (true)
+                {
+                    socketSend = socketWatch.Accept();
+                    //将远程连接的Ip地址和连接存入集合中
+                    dic.Add(socketSend.RemoteEndPoint.ToString(), socketSend);
+                    cobIPList.Items.Add(socketSend.RemoteEndPoint.ToString());
+                    //  ShowMsg(socketWatch.RemoteEndPoint.ToString() + ":" + "连接成功！");
 
-                Thread th = new Thread(Recviec);
-                th.IsBackground = true;
-                th.Start(socketSend);
+                    Thread th = new Thread(Recviec);
+                    th.IsBackground = true;
+                    th.Start(socketSend);
+                }
+            }
+            catch (Exception)
+            {
 
             }
         }
@@ -66,16 +79,23 @@ namespace SocketServer
         /// <param name="obj"></param>
         void Recviec(object obj)
         {
-            Socket socketSend = obj as Socket;
-            while (true)
+            try
             {
-                //客户端连接成功后，服务端应接受客户端发送多来的消息
-                byte[] buffer = new byte[1024 * 1024 * 2];
-                //实际接收到的文件字节数
-                int r = socketSend.Receive(buffer);
-                if (r == 0) { break; }
-                string str = Encoding.UTF8.GetString(buffer, 0, r);
-                ShowMsg(socketSend.RemoteEndPoint + ":" + str);
+                Socket socketSend = obj as Socket;
+                while (true)
+                {
+                    //客户端连接成功后，服务端应接受客户端发送多来的消息
+                    byte[] buffer = new byte[1024 * 1024 * 2];
+                    //实际接收到的文件字节数
+                    int r = socketSend.Receive(buffer);
+                    if (r == 0) { break; }
+                    string str = Encoding.UTF8.GetString(buffer, 0, r);
+                    ShowMsg(socketSend.RemoteEndPoint + ":" + str);
+                }
+            }
+            catch (Exception)
+            {
+
             }
         }
 
@@ -95,60 +115,75 @@ namespace SocketServer
         /// <param name="e"></param>
         private void btnSendMsg_Click(object sender, EventArgs e)
         {
-            string ip = cobIPList.SelectedItem.ToString();
-            byte[] buffer = Encoding.UTF8.GetBytes(txtReceiveMsg.Text.Trim());
-            List<byte> list = new List<byte>();
-            list.Add(0);
-            list.AddRange(buffer);
-            dic[ip].Send(list.ToArray());
+            try
+            {
+                string ip = cobIPList.SelectedItem.ToString();
+                byte[] buffer = Encoding.UTF8.GetBytes(txtReceiveMsg.Text.Trim());
+                List<byte> list = new List<byte>();
+                list.Add(0);
+                list.AddRange(buffer);
+                dic[ip].Send(list.ToArray());
+            }
+            catch (Exception)
+            {
+
+            }
         }
 
 
         private void btnSelect_Click(object sender, EventArgs e)
         {
-            OpenFileDialog open = new OpenFileDialog();
-            open.Title = "请选择要发送的文件";
-            open.InitialDirectory = @"C:\Users\Administrator\Desktop";
-            open.Filter = "所有文件|*.*";
-            open.ShowDialog();
+            try
+            {
+                OpenFileDialog open = new OpenFileDialog();
+                open.Title = "请选择要发送的文件";
+                open.InitialDirectory = @"C:\Users\Administrator\Desktop";
+                open.Filter = "所有文件|*.*";
+                open.ShowDialog();
 
-            txtPath.Text = open.FileName;
+                txtPath.Text = open.FileName;
+            }
+            catch (Exception)
+            {
+
+            }
         }
 
         private void btnSendFile_Click(object sender, EventArgs e)
         {
-            //获得要发送文件的路径
-            string path = txtPath.Text.Trim();
-            using (FileStream fs = new FileStream(path, FileMode.Open, FileAccess.Read))
+            try
             {
-                byte[] buffer = new byte[1024 * 1024 * 5];
-                int r = fs.Read(buffer, 0, buffer.Length);
-                List<byte> list = new List<byte>();
-                list.Add(1);
-                list.AddRange(buffer);
-                dic[socketSend.RemoteEndPoint.ToString()].Send(list.ToArray(), 0, r+1 , SocketFlags.None);
+                //获得要发送文件的路径
+                string path = txtPath.Text.Trim();
+                string ip = cobIPList.SelectedItem.ToString();
+                using (FileStream fs = new FileStream(path, FileMode.Open, FileAccess.Read))
+                {
+                    byte[] buffer = new byte[1024 * 1024 * 5];
+                    int r = fs.Read(buffer, 0, buffer.Length);
+                    List<byte> list = new List<byte>();
+                    list.Add(1);
+                    list.AddRange(buffer);
+                    dic[ip].Send(list.ToArray(), 0, r + 1, SocketFlags.None);
+                }
+            }
+            catch (Exception)
+            {
+
             }
         }
 
         private void btnShock_Click(object sender, EventArgs e)
         {
-            byte[] buffer= new byte[1];
-            buffer[0] = 2;
-            dic[socketSend.RemoteEndPoint.ToString()].Send(buffer);
-        }
-
-        private void button1_Click(object sender, EventArgs e)
-        {
-            this.Invoke(new System.Windows.Forms.MethodInvoker(delegate() 
+            try
             {
-                SaveFileDialog ssd = new SaveFileDialog();
-                ssd.InitialDirectory = @"C:\Users\Administrator\Desktop";
-                ssd.Title = "请选择要发送的文件";
-                ssd.Filter = "所有文件|*.*";
-                ssd.ShowDialog();
-            }));
-
-
+                string ip = cobIPList.SelectedItem.ToString();
+                byte[] buffer = new byte[1];
+                buffer[0] = 2;
+                dic[ip].Send(buffer);
+            }
+            catch (Exception)
+            {
+            }
         }
     }
 }
